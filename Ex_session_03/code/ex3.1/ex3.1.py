440406234b42cfe69bdd0ba325f3deed760018ab
ing.conti <ingconti@ingconti.com>
2020-11-13 17:26:00 +0100

    added ex3.1 && ex 3.2-

---
+++ Ex_session_03/code/ex3.1/ex3.1.py
@@ -0,0 +1,28 @@
+
+FNAME = "samples.csv"
+
+f=open(FNAME)
+
+lines = f.read().splitlines() #prevent be user has typed return.
+lineCount = len(lines)
+if lineCount > 0:
+	line = lines[0]		#take first only
+	elementsList = line.split(',')
+	elementsCount = len(elementsList)
+	sum = 0
+	if elementsCount>0:
+		max = int(elementsList[0])
+		min = int(elementsList[0])
+		for i in range(1, elementsCount):
+			value = int(elementsList[i])
+			sum += value
+			if value >max:
+				max = value
+			if value < min:
+				min = value
+		print(sum, max, min)
+	else:
+		print(False)
+
+else:
+	print(False)
