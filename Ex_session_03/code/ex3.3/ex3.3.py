2300ecaa2eb4db37dea80e9cd368dc5e50e101a9
ing.conti <ingconti@ingconti.com>
2020-11-15 10:40:35 +0100

    added dicts

---
+++ Ex_session_03/code/ex3.3/ex3.3.py
@@ -0,0 +1,28 @@
+iso1 = str(input('1st ISO code '))
+iso2 = str(input('2nd ISO code '))
+
+isoSet = {iso1, iso2}  # build a set.
+
+FNAME = "TB_burden_age_sex_2020-11-05.csv"
+#FNAME = "TB_burden_reduced.csv"
+f=open(FNAME)
+lines = f.read().splitlines()
+nOfLines = len(lines)
+if nOfLines > 0:
+	sum = 0
+	titleRow = lines[0].replace('"', '')
+	columnsTitles = titleRow.split(',')
+	columnOfBest =  columnsTitles.index('best')
+	columnOfISOCode = columnsTitles.index('iso2')
+	columnOfSex = columnsTitles.index('sex')
+	for i in range(1, len(lines)):
+		line = lines[i].replace('"', '')
+		columnsValues = line.split(',')
+		a = columnsValues[columnOfISOCode]
+		b = columnsValues[columnOfSex]
+
+		if columnsValues[columnOfISOCode] in isoSet and columnsValues[columnOfSex] == 'f':
+			sum = sum + int(columnsValues[columnOfBest])
+	print(sum)
+else:
+	print(False)
